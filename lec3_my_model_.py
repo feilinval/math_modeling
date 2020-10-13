b1180ee66dd94df94863da08faf19b3c20f1f151
feilinval <72086981+feilinval@users.noreply.github.com>
2020-10-13 17:45:07 +0200

    49930130

---
+++ lec3_my_model_.py
@@ -0,0 +1,12 @@
+a=3
+b=55
+c=1
+d=5
+
+eath_mass = 5.94*10*24
+gravity_constant = 6.67*10**(-11)
+your_mam_mass = 100000**100000
+
+from lec3_my_model_ import gravity_constant as K
+g = 500*K/10*2
+print (g)
