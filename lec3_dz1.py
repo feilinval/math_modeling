04f81a3109bd6ac619f1736c155d3502c4368e6c
feilinval <72086981+feilinval@users.noreply.github.com>
2020-10-15 17:06:02 +0200

    1111

---
+++ lec3_dz1.py
@@ -0,0 +1,11 @@
+gravity_constant = 6.67*10**(-11)
+skorost_sveta = 3*10**8
+massa_neitrina=1.675*10**(-27)
+eath_mass = 5.94*10*24
+uskorenie_svobodnogo_padenia = 9.8
+massa_tritona = 1.67*10**(-27)
+elemantarnij_zarjd = 1.6*10**(-19)
+postojanaj_planka = 6.63*10**(-34)
+postojanaj_bolzmana = 1.38*10**(-23)
+postojanaj_avogadro = 6.2*10**23
+
