db095428ea6ec80adc9c2d314bd80161beda124d
feilinval <72086981+feilinval@users.noreply.github.com>
2020-11-10 17:45:21 +0200

    732424

---
+++ less4_dop_dz1.py
@@ -0,0 +1,12 @@
+def mult_func(a,n):
+    an=1
+    an=a*an
+    return an
+    for i in range(n):
+        range(start,stop,step)
+        print(an)
+        print(range,(0,3,1)
+        
+
+        
+
